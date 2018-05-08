5a28b51e46d91c3bf793ae374affde8f1fefc546
susu <yingxue.su@radida.cn>
2018-05-08 15:07:24 +0800

    share app message

--- pages/lookResume/lookResume.js
+++ pages/lookResume/lookResume.js
@@ -20,6 +20,15 @@ Page({
                 'userName': queryParams.userName
             })
         }
+        if (!this.data.shareResumeId) {
+            sdkApi.findBaseInfo({shareResumeId: app.globalData.loginInfo.openid}, res => {
+                console.log('11111111111111111111----res', res)
+                this.setData({
+                    userName: res.objects[0].userName
+                })
+                console.log('22222222222222------', this.data.userName)
+            })
+        }
         
     },
     onShow: function () {
@@ -31,7 +40,7 @@ Page({
     
     findCollectResume () {
         sdkApi.findCollectResume({}, res => {
-            console.log('0000000000000-------res', res)
+            // console.log('0000000000000-------res', res)
             if (res.objects[0].id) {
                 let currentResume = this.data.shareResumeId + ',' + this.data.userName
 
@@ -50,45 +59,46 @@ Page({
                 })
             }
         })
-        console.log('66666666666-----tapAdd', this.data.tapAdd)
+        // console.log('66666666666-----tapAdd', this.data.tapAdd)
     },
 
     // share your remuse
     onShareAppMessage: function (res) {
-        // if (res.from === 'button') {
-        //     // 来自页面内转发按钮
-        //     console.log('来自页面内转发按钮------res.target', res.target)
-        //   }
         if (!this.data.shareResumeId) {
-            sdkApi.findBaseInfo({shareResumeId: this.data.shareResumeId}, res => {
-                console.log('66666666666------res', res)
-                  return {
-                    title: 'my resume',
-                    path: '/pages/lookResume/lookResume?shareResumeId=' + app.globalData.loginInfo.openid + '/userName=' + res.objects[0].userName,
-                    success: function(res) {
-                      // 转发成功
-                    //   wx.showShareMenu({
-                    //      // 要求小程序返回分享目标信息
-                    //      withShareTicket: true
-                    //   })
-                    },
-                    fail: function(res) {
-                      // 转发失败
-                      wx.showModal({
-                        title: 'share failures',
-                        content: 'please improve your information first',
+                // if (this.data.userName) {
+                    console.log('5555555555555------', this.data.userName)
+                    return {
+                        title: 'my resume',
+                        path: '/pages/lookResume/lookResume?shareResumeId=' + app.globalData.loginInfo.openid + '&userName=' + this.data.userName,
                         success: function(res) {
-                          if (res.confirm) { // click confirm
-                            wx.navigateTo({
-                                url: '/pages/editResume/editResume'
-                              })
-                          }
+                          // 转发成功
+                        //   wx.showShareMenu({
+                        //      // 要求小程序返回分享目标信息
+                        //      withShareTicket: true
+                        //   })
+                        },
+                        fail: function(res) {
+                          // 转发失败
                         }
-                      })
-                    }
-                  }
-            })
+                      }
+                // } 
+                // else {
+                //     console.log('66666666666------')
+                //     wx.showModal({
+                //         title: 'share failures',
+                //         content: 'please improve your information first',
+                //         success: function(res) {
+                //           if (res.confirm) { // click confirm
+                //             wx.navigateTo({
+                //                 url: '/pages/editResume/editResume'
+                //               })
+                //           }
+                //         }
+                //       })
+                // }
+                  
         } else {
+            console.log('333333333333333333333333')
               return {
                 title: 'other ruseme',
                 path: '/pages/lookResume/lookResume?shareResumeId=' + this.data.shareResumeId + '&userName=' + this.data.userName
@@ -101,13 +111,13 @@ Page({
     },
 
     collectResumeTap () {
-        console.log('6666666666666------this.data.collectResume', this.data.collectResume)
+        // console.log('6666666666666------this.data.collectResume', this.data.collectResume)
         let params = this.data.shareResumeId + ',' + this.data.userName
         if (this.data.collectResume) {
             if (this.data.tapAdd) {
                 sdkApi.addCollectResume({recordID: this.data.recordID, collectInfo: [params]}, res => {
                     this.findCollectResume()
-                    console.log('66666666666666-----res', res)
+                    // console.log('66666666666666-----res', res)
                     this.setData({
                         'tapAdd': false,
                         'collectResume': !this.data.collectResume
@@ -116,7 +126,7 @@ Page({
                 })
             } else {
                 sdkApi.uAppendCollectResume({recordID: this.data.recordID, collectInfo: params }, res => {
-                    console.log('66666666666666---update-----res', res)
+                    // console.log('66666666666666---update-----res', res)
                     this.setData({
                         'collectResume': !this.data.collectResume
                     })
@@ -127,7 +137,7 @@ Page({
             
         } else {
             sdkApi.removeCollectResume({recordID: this.data.recordID, collectInfo: params}, res => {
-                console.log('66666666666666---remove---res', res)
+                // console.log('66666666666666---remove---res', res)
                 this.setData({
                     'collectResume': !this.data.collectResume
                 })
@@ -138,25 +148,21 @@ Page({
     },
 
     baseInfoTap () {
-        console.log('6666666666666---baseInfoTap')
         wx.navigateTo({
             url: '/pages/baseInfo/baseInfo?shareResumeId=' + this.data.shareResumeId
         })
     },
     workInfoTap () {
-        console.log('55555555555555---workInfoTap')
         wx.navigateTo({
             url: '/pages/workInfo/workInfo?shareResumeId=' + this.data.shareResumeId
         })
     },
     otherInfoTap () {
-        console.log('4444444444444444---otherInfoTap')
         wx.navigateTo({
             url: '/pages/otherInfo/otherInfo?shareResumeId=' + this.data.shareResumeId
         })
     },
     selfIntroductionTap () {
-        console.log('33333333333333---selfIntroductionTap')
         wx.navigateTo({
             url: '/pages/selfIntroduction/selfIntroduction?shareResumeId=' + this.data.shareResumeId
         })
